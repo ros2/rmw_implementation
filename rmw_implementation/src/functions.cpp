cbb6539a08497ff7c2154572697c47a275c2103c
Mikael Arguedas <mikael@osrfoundation.org>
2017-05-07 12:49:20 -0400

    use rmw_node_security_options structure

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -184,12 +184,12 @@ rmw_init(void)
 
 rmw_node_t *
 rmw_create_node(const char * name, const char * namespace_, size_t domain_id,
-  bool enforce_security, const char * security_root_path)
+  const rmw_node_security_options_t * options)
 {
   CALL_SYMBOL(
     "rmw_create_node", rmw_node_t *, nullptr,
-    ARG_TYPES(const char *, const char *, size_t, bool, const char *),
-    ARG_VALUES(name, namespace_, domain_id, enforce_security, security_root_path));
+    ARG_TYPES(const char *, const char *, size_t, const rmw_node_security_options_t *),
+    ARG_VALUES(name, namespace_, domain_id, options));
 }
 
 rmw_ret_t
