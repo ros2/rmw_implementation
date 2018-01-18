8fa3cfe1f59f3226223018712d1be7e9a25ba27a
Sriram Raghunathan <sriram.max@gmail.com>
2018-01-18 23:40:01 +0530

    API to enable log severity setting.  (#30)
    * The patch follows the discussion at this
    
    https://github.com/ros2/rmw/pull/124/ url on rmw.
    
    The patch enables setting log severity from userland applications. The parameter
    passed is defined inside rmw/include/types.h
    
    Signed-off-by: Sriram Raghunathan <sriram.max@gmail.com>
    
    * Changing rmw_log_severity_t from const * to rmw_log_severity_t
    addressing comments
    
    Signed-off-by: Sriram Raghunathan <sriram.max@gmail.com>

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -355,6 +355,10 @@ RMW_INTERFACE_FN(rmw_service_server_is_available,
   rmw_ret_t, RMW_RET_ERROR,
   3, ARG_TYPES(const rmw_node_t *, const rmw_client_t *, bool *))
 
+RMW_INTERFACE_FN(rmw_set_log_severity,
+  rmw_ret_t, RMW_RET_ERROR,
+  1, ARG_TYPES(rmw_log_severity_t))
+
 #define GET_SYMBOL(x) symbol_ ## x = get_symbol(#x);
 
 void prefetch_symbols(void)
