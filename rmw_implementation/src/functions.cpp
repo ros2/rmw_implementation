ac7a732daac8b20ac6473c8582f4da9dbbfd51d4
Karsten Knese <karsten.knese@googlemail.com>
2018-06-14 02:04:52 +0200

    raw->serialized

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -246,17 +246,17 @@ RMW_INTERFACE_FN(rmw_publish,
   rmw_ret_t, RMW_RET_ERROR,
   2, ARG_TYPES(const rmw_publisher_t *, const void *))
 
-RMW_INTERFACE_FN(rmw_publish_raw,
+RMW_INTERFACE_FN(rmw_publish_serialized_message,
   rmw_ret_t, RMW_RET_ERROR,
-  2, ARG_TYPES(const rmw_publisher_t *, const rmw_message_raw_t *))
+  2, ARG_TYPES(const rmw_publisher_t *, const rmw_serialized_message_t *))
 
 RMW_INTERFACE_FN(rmw_serialize,
   rmw_ret_t, RMW_RET_ERROR,
-  3, ARG_TYPES(const void *, const rosidl_message_type_support_t *, rmw_message_raw_t *))
+  3, ARG_TYPES(const void *, const rosidl_message_type_support_t *, rmw_serialized_message_t *))
 
 RMW_INTERFACE_FN(rmw_deserialize,
   rmw_ret_t, RMW_RET_ERROR,
-  3, ARG_TYPES(const rmw_message_raw_t *, const rosidl_message_type_support_t *, void *))
+  3, ARG_TYPES(const rmw_serialized_message_t *, const rosidl_message_type_support_t *, void *))
 
 RMW_INTERFACE_FN(rmw_create_subscription,
   rmw_subscription_t *, nullptr,
@@ -276,13 +276,14 @@ RMW_INTERFACE_FN(rmw_take_with_info,
   rmw_ret_t, RMW_RET_ERROR,
   4, ARG_TYPES(const rmw_subscription_t *, void *, bool *, rmw_message_info_t *))
 
-RMW_INTERFACE_FN(rmw_take_raw,
+RMW_INTERFACE_FN(rmw_take_serialized_message,
   rmw_ret_t, RMW_RET_ERROR,
-  3, ARG_TYPES(const rmw_subscription_t *, rmw_message_raw_t *, bool *))
+  3, ARG_TYPES(const rmw_subscription_t *, rmw_serialized_message_t *, bool *))
 
-RMW_INTERFACE_FN(rmw_take_raw_with_info,
+RMW_INTERFACE_FN(rmw_take_serialized_message_with_info,
   rmw_ret_t, RMW_RET_ERROR,
-  4, ARG_TYPES(const rmw_subscription_t *, rmw_message_raw_t *, bool *, rmw_message_info_t *))
+  4, ARG_TYPES(
+    const rmw_subscription_t *, rmw_serialized_message_t *, bool *, rmw_message_info_t *))
 
 RMW_INTERFACE_FN(rmw_create_client,
   rmw_client_t *, nullptr,
@@ -399,15 +400,15 @@ void prefetch_symbols(void)
   GET_SYMBOL(rmw_create_publisher)
   GET_SYMBOL(rmw_destroy_publisher)
   GET_SYMBOL(rmw_publish)
-  GET_SYMBOL(rmw_publish_raw)
+  GET_SYMBOL(rmw_publish_serialized_message)
   GET_SYMBOL(rmw_serialize)
   GET_SYMBOL(rmw_deserialize)
   GET_SYMBOL(rmw_create_subscription)
   GET_SYMBOL(rmw_destroy_subscription)
   GET_SYMBOL(rmw_take)
   GET_SYMBOL(rmw_take_with_info)
-  GET_SYMBOL(rmw_take_raw)
-  GET_SYMBOL(rmw_take_raw_with_info)
+  GET_SYMBOL(rmw_take_serialized_message)
+  GET_SYMBOL(rmw_take_serialized_message_with_info)
   GET_SYMBOL(rmw_create_client)
   GET_SYMBOL(rmw_destroy_client)
   GET_SYMBOL(rmw_send_request)
