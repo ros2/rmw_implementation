8097e614680752317a6b87386fce19c5d6627102
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-04-03 10:14:18 -0300

    Use one participant per context API changes (#77)
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -190,9 +190,8 @@ RMW_INTERFACE_FN(
 RMW_INTERFACE_FN(
   rmw_create_node,
   rmw_node_t *, nullptr,
-  6, ARG_TYPES(
-    rmw_context_t *, const char *, const char *, size_t, const rmw_node_security_options_t *,
-    bool))
+  5, ARG_TYPES(
+    rmw_context_t *, const char *, const char *, size_t, bool))
 
 RMW_INTERFACE_FN(
   rmw_destroy_node,
@@ -518,6 +517,13 @@ RMW_INTERFACE_FN(
   3, ARG_TYPES(const rmw_node_t *, rcutils_string_array_t *, rcutils_string_array_t *))
 
 RMW_INTERFACE_FN(
+  rmw_get_node_names_with_security_contexts,
+  rmw_ret_t, RMW_RET_ERROR,
+  4, ARG_TYPES(
+    const rmw_node_t *, rcutils_string_array_t *,
+    rcutils_string_array_t *, rcutils_string_array_t *))
+
+RMW_INTERFACE_FN(
   rmw_count_publishers,
   rmw_ret_t, RMW_RET_ERROR,
   3, ARG_TYPES(const rmw_node_t *, const char *, size_t *))
@@ -636,6 +642,7 @@ void prefetch_symbols(void)
   GET_SYMBOL(rmw_get_topic_names_and_types)
   GET_SYMBOL(rmw_get_service_names_and_types)
   GET_SYMBOL(rmw_get_node_names)
+  GET_SYMBOL(rmw_get_node_names_with_security_contexts)
   GET_SYMBOL(rmw_count_publishers)
   GET_SYMBOL(rmw_count_subscribers)
   GET_SYMBOL(rmw_get_gid_for_publisher)
