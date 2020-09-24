f49046bdb512c1cafad53742e44dd5fee438dbea
lobotuerk <jtlorente@ekumenlabs.com>
2020-09-24 11:31:11 -0300

    fix merge
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

--- test_rmw_implementation/test/test_subscription.cpp
+++ test_rmw_implementation/test/test_subscription.cpp
@@ -386,6 +386,41 @@ TEST_F(CLASSNAME(TestSubscriptionUse, RMW_IMPLEMENTATION), count_mismatched_subs
   EXPECT_EQ(0u, publisher_count);
 }
 
+TEST_F(CLASSNAME(TestSubscriptionUse, RMW_IMPLEMENTATION), take_with_bad_args) {
+  bool taken = false;
+  test_msgs__msg__BasicTypes output_message{};
+  output_message.bool_value = true;
+  output_message.char_value = 'a';
+  output_message.float32_value = 0.42;
+  test_msgs__msg__BasicTypes original_message = output_message;
+  rmw_subscription_allocation_t * null_allocation{nullptr};  // still valid allocation
+
+  rmw_ret_t ret = rmw_take(nullptr, &output_message, &taken, null_allocation);
+  EXPECT_EQ(RMW_RET_INVALID_ARGUMENT, ret) << rmw_get_error_string().str;
+  EXPECT_EQ(taken, false);
+  EXPECT_EQ(output_message, original_message);
+  rmw_reset_error();
+
+  ret = rmw_take(sub, nullptr, &taken, null_allocation);
+  EXPECT_EQ(RMW_RET_INVALID_ARGUMENT, ret) << rmw_get_error_string().str;
+  EXPECT_EQ(taken, false);
+  rmw_reset_error();
+
+  ret = rmw_take(sub, &output_message, nullptr, null_allocation);
+  EXPECT_EQ(RMW_RET_INVALID_ARGUMENT, ret) << rmw_get_error_string().str;
+  EXPECT_EQ(output_message, original_message);
+  rmw_reset_error();
+
+  const char * implementation_identifier = sub->implementation_identifier;
+  sub->implementation_identifier = "not-an-rmw-implementation-identifier";
+  ret = rmw_take(sub, &output_message, &taken, null_allocation);
+  EXPECT_EQ(RMW_RET_INCORRECT_RMW_IMPLEMENTATION, ret) << rmw_get_error_string().str;
+  EXPECT_EQ(taken, false);
+  EXPECT_EQ(output_message, original_message);
+  rmw_reset_error();
+  sub->implementation_identifier = implementation_identifier;
+}
+
 TEST_F(CLASSNAME(TestSubscriptionUse, RMW_IMPLEMENTATION), take_with_info_with_bad_args) {
   bool taken = false;
   test_msgs__msg__BasicTypes output_message{};
