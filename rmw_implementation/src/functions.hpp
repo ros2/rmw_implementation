7bc420a65aca0a3cd2d25faa7f3986ccc3d4bc77
Michel Hidalgo <michel@ekumenlabs.com>
2020-09-29 11:31:59 -0300

    Fix up C functions to never throw.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rmw_implementation/src/functions.hpp
+++ rmw_implementation/src/functions.hpp
@@ -25,7 +25,7 @@ RMW_IMPLEMENTATION_DEFAULT_VISIBILITY
 std::shared_ptr<rcpputils::SharedLibrary> load_library();
 
 RMW_IMPLEMENTATION_DEFAULT_VISIBILITY
-void * lookup_symbol(std::shared_ptr<rcpputils::SharedLibrary> lib, const char * symbol_name);
+void * lookup_symbol(std::shared_ptr<rcpputils::SharedLibrary> lib, const std::string & symbol_name);
 
 #ifdef __cplusplus
 extern "C"
