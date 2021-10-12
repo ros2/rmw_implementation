5dcf45ab7a5b63371b4eedaf16c635319ab92c43
Chen Lihui <lihui.chen@sony.com>
2021-10-12 09:57:08 +0800

    Update interface
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -417,12 +417,15 @@ RMW_INTERFACE_FN(
 RMW_INTERFACE_FN(
   rmw_subscription_set_cft_expression_parameters,
   rmw_ret_t, RMW_RET_ERROR,
-  3, ARG_TYPES(rmw_subscription_t *, const char *, const rcutils_string_array_t *))
+  2, ARG_TYPES(
+    rmw_subscription_t *, const rmw_subscription_content_filtered_topic_options_t *))
 
 RMW_INTERFACE_FN(
   rmw_subscription_get_cft_expression_parameters,
   rmw_ret_t, RMW_RET_ERROR,
-  3, ARG_TYPES(const rmw_subscription_t *, char **, rcutils_string_array_t *))
+  3, ARG_TYPES(
+    const rmw_subscription_t *, rcutils_allocator_t *,
+    rmw_subscription_content_filtered_topic_options_t *))
 
 RMW_INTERFACE_FN(
   rmw_take,
