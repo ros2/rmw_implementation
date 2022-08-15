43a0f8dee550f4fc7a7a9c1c6a9bb42d2cc4370d
Brian Chen <brian.chen@openrobotics.org>
2022-08-15 11:57:39 -0700

    add rmw_get_gid_for_client
    Signed-off-by: Brian Chen <brian.chen@openrobotics.org>

--- test_rmw_implementation/test/test_unique_identifiers.cpp
+++ test_rmw_implementation/test/test_unique_identifiers.cpp
@@ -57,6 +57,9 @@ protected:
     constexpr char topic_name[] = "/test0";
     pub = rmw_create_publisher(node, ts, topic_name, &qos_profile, &options);
     ASSERT_NE(nullptr, pub) << rmw_get_error_string().str;
+
+    constexpr char service_name[] = "test_service";
+    cli = rmw_create_client(node, srv_ts, service_name, &qos_profile);
   }
 
   void TearDown() override
@@ -75,11 +78,14 @@ protected:
   rmw_node_t * node{nullptr};
   const rosidl_message_type_support_t * ts{
     ROSIDL_GET_MSG_TYPE_SUPPORT(test_msgs, msg, BasicTypes)};
+  const rosidl_service_type_support_t * srv_ts{
+    ROSIDL_GET_SRV_TYPE_SUPPORT(test_msgs, srv, BasicTypes)};
   rmw_qos_profile_t qos_profile{rmw_qos_profile_default};
   rmw_publisher_t * pub{nullptr};
+  rmw_client_t * cli{nullptr};
 };
 
-TEST_F(CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION), get_gid_with_bad_args) {
+TEST_F(CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION), get_pub_gid_with_bad_args) {
   rmw_gid_t gid{};
   rmw_ret_t ret = rmw_get_gid_for_publisher(pub, &gid);
   ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
@@ -105,6 +111,41 @@ TEST_F(CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION), get_gid_with_bad_
   pub->implementation_identifier = "not-an-rmw-implementation-identifier";
   ret = rmw_get_gid_for_publisher(pub, &gid);
   pub->implementation_identifier = implementation_identifier;
+  EXPECT_EQ(RMW_RET_INCORRECT_RMW_IMPLEMENTATION, ret);
+  rmw_reset_error();
+  gids_are_equal = false;
+  ret = rmw_compare_gids_equal(&expected_gid, &gid, &gids_are_equal);
+  EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+  EXPECT_TRUE(gids_are_equal);
+}
+
+
+TEST_F(CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION), get_cli_gid_with_bad_args) {
+  rmw_gid_t gid{};
+  rmw_ret_t ret = rmw_get_gid_for_client(cli, &gid);
+  ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+  rmw_gid_t expected_gid = gid;
+
+  ret = rmw_get_gid_for_client(nullptr, &gid);
+  EXPECT_EQ(RMW_RET_INVALID_ARGUMENT, ret);
+  rmw_reset_error();
+  bool gids_are_equal = false;
+  ret = rmw_compare_gids_equal(&expected_gid, &gid, &gids_are_equal);
+  EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+  EXPECT_TRUE(gids_are_equal);
+
+  ret = rmw_get_gid_for_client(cli, nullptr);
+  EXPECT_EQ(RMW_RET_INVALID_ARGUMENT, ret);
+  rmw_reset_error();
+  gids_are_equal = false;
+  ret = rmw_compare_gids_equal(&expected_gid, &gid, &gids_are_equal);
+  EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+  EXPECT_TRUE(gids_are_equal);
+
+  const char * implementation_identifier = cli->implementation_identifier;
+  cli->implementation_identifier = "not-an-rmw-implementation-identifier";
+  ret = rmw_get_gid_for_client(cli, &gid);
+  cli->implementation_identifier = implementation_identifier;
   EXPECT_EQ(RMW_RET_INCORRECT_RMW_IMPLEMENTATION, ret);
   rmw_reset_error();
   gids_are_equal = false;
@@ -221,3 +262,56 @@ TEST_F(CLASSNAME(TestUniqueIdentifiersForMultiplePublishers, RMW_IMPLEMENTATION)
   EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
   EXPECT_FALSE(are_equal);
 }
+
+
+class CLASSNAME (TestUniqueIdentifiersForMultipleClients, RMW_IMPLEMENTATION)
+  : public CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION)
+{
+protected:
+  using Base = CLASSNAME(TestUniqueIdentifierAPI, RMW_IMPLEMENTATION);
+  void SetUp() override
+  {
+    Base::SetUp();
+    rmw_publisher_options_t options = rmw_get_default_publisher_options();
+    constexpr char service_name[] = "service_name";
+    cli1 = rmw_create_client(
+      node, srv_ts, service_name, &qos_profile);
+    ASSERT_NE(nullptr, cli1) << rmw_get_error_string().str;
+    cli2 = rmw_create_client(
+      node, srv_ts, service_name, &qos_profile);
+    ASSERT_NE(nullptr, cli2) << rmw_get_error_string().str;
+  }
+
+  void TearDown() override
+  {
+    rmw_ret_t ret = rmw_destroy_client(node, cli1);
+    EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+    ret = rmw_destroy_client(node, cli2);
+    EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+    Base::TearDown();
+  }
+
+  rmw_client_t * cli1 {nullptr};
+  rmw_client_t * cli2 {nullptr};
+};
+
+
+TEST_F(CLASSNAME(TestUniqueIdentifiersForMultipleClients, RMW_IMPLEMENTATION), different_clis) {
+  rmw_gid_t cli1_gid{};
+  rmw_gid_t cli2_gid{};
+
+  rmw_ret_t ret = rmw_get_gid_for_client(cli1, &cli1_gid);
+  ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+
+  ret = rmw_get_gid_for_client(cli2, &cli2_gid);
+  ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+
+  bool are_equal = true;
+  ret = rmw_compare_gids_equal(&cli1_gid, &cli2_gid, &are_equal);
+  EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
+  EXPECT_FALSE(are_equal);
+}
+
+
+
+
