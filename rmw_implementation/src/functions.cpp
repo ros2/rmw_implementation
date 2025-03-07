1cd4dab4521cd4a3b43b9ab04065493d96a00531
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-03-07 17:42:38 +0100

    updated name and use in wait_set test
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rmw_implementation/src/functions.cpp
+++ rmw_implementation/src/functions.cpp
@@ -758,7 +758,7 @@ RMW_INTERFACE_FN(
     rmw_feature_t))
 
 RMW_INTERFACE_FN(
-  rmw_event_check_compatible,
+  rmw_event_type_is_supported,
   bool, RMW_EVENT_INVALID,
   1, ARG_TYPES(
     rmw_event_type_t))
@@ -847,7 +847,7 @@ void prefetch_symbols(void)
   GET_SYMBOL(rmw_take_request)
   GET_SYMBOL(rmw_send_response)
   GET_SYMBOL(rmw_take_event)
-  GET_SYMBOL(rmw_event_check_compatible)
+  GET_SYMBOL(rmw_event_type_is_supported)
   GET_SYMBOL(rmw_create_guard_condition)
   GET_SYMBOL(rmw_destroy_guard_condition)
   GET_SYMBOL(rmw_trigger_guard_condition)
@@ -1006,7 +1006,7 @@ unload_library()
   symbol_rmw_client_set_on_new_response_callback = nullptr;
   symbol_rmw_event_set_callback = nullptr;
   symbol_rmw_feature_supported = nullptr;
-  symbol_rmw_event_check_compatible = nullptr;
+  symbol_rmw_event_type_is_supported = nullptr;
   symbol_rmw_take_dynamic_message = nullptr;
   symbol_rmw_take_dynamic_message_with_info = nullptr;
   symbol_rmw_serialization_support_init = nullptr;
--- test_rmw_implementation/test/test_wait_set.cpp
+++ test_rmw_implementation/test/test_wait_set.cpp
@@ -119,7 +119,12 @@ protected:
     ASSERT_NE(nullptr, srv) << rmw_get_error_string().str;
     client = rmw_create_client(node, service_ts, service_name, &rmw_qos_profile_default);
     ASSERT_NE(nullptr, client) << rmw_get_error_string().str;
-    rmw_ret_t ret = rmw_subscription_event_init(&event, sub, RMW_EVENT_LIVELINESS_CHANGED);
+    rmw_ret_t ret = RMW_RET_OK;
+    if (!rmw_event_type_is_supported(RMW_EVENT_LIVELINESS_CHANGED)) {
+      ret = rmw_subscription_event_init(&event, sub, RMW_EVENT_PUBLICATION_MATCHED);
+    } else {
+      ret = rmw_subscription_event_init(&event, sub, RMW_EVENT_LIVELINESS_CHANGED);
+    }
     ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
   }
 
